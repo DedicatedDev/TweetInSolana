2cd4ee1ce6a59e79a94bf7a270161711351cf31b
DedicatedDev <topdev323@gmail.com>
2022-04-02 05:29:33 +0300

    staking

--- programs/vote/src/data.rs
+++ programs/vote/src/data.rs
@@ -2,7 +2,8 @@
 use anchor_lang::prelude::*;
 #[derive(Accounts)]
 pub struct  VotePlatform<'info> {
-    #[account(init, payer = user, space = 9000)]
+    //#[account(init, payer = user, space = 9000)]
+    #[account(mut)]
     pub candidate: Account<'info, Candidate>,
     #[account(mut)]
     pub user: Signer<'info>,
